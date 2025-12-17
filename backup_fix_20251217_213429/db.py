ecfaaeed29539ff03f23d5f647027d5ced8f7da0
Your Name <abbedakbery14@gmail.com>
2025-12-18 08:45:23 +1000

    Clean repo (ignore venv/logs/backups)

--- backup_fix_20251217_213429/db.py
+++
@@ -1,8 +0,0 @@
-﻿import psycopg
-from pgvector.psycopg import register_vector
-from .settings import settings
-
-def get_conn():
-    conn = psycopg.connect(settings.DATABASE_URL)
-    register_vector(conn)
-    return conn
